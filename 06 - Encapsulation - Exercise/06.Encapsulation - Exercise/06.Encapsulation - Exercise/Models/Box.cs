19baec7989b119db08b28e86d292a8d5ca7c38ae
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-02-28 15:37:38 +0200

    01. Class Box Data 75% DONE

---
+++ 06 - Encapsulation - Exercise/06.Encapsulation - Exercise/06.Encapsulation - Exercise/Models/Box.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+
+namespace Encapsulation.Models
+{
+    public class Box
+    {
+		private const string ArgumentExceptionError = "{0} cannot be zero or negative.";
+
+		private double lenght;
+		private double width;
+		private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+         } 
+		public double Lenght
+		{
+			get { return lenght; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ArgumentExceptionError, nameof(Lenght)));
+
+                }
+                lenght = value;
+            }
+        }
+		public double Width
+		{
+			get { return width; }
+			private set
+			{
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ArgumentExceptionError, nameof(Width)));
+
+                }
+                width = value;
+			}
+		}
+
+        public double Height
+		{
+			get { return height; }
+			private set 
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException(string.Format(ArgumentExceptionError, nameof(Height)));
+
+                }
+				height = value; 
+			}
+		}
+
+
+
+
+        public double SurfaceArea()
+        {
+            return 2*((Lenght * Width) + (Lenght * Height) + (Width * Height));
+        }
+
+        public double LateralSurfaceArea()
+        {  
+            return 2 * Lenght * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Width * Height * Lenght;
+        }
+    }
+}
---
+++ 06 - Encapsulation - Exercise/06.Encapsulation - Exercise/06.Encapsulation - Exercise/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using Encapsulation.Models;
+
+namespace Encapsulation
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double surfaceArea = 0 ;
+            double lateralSurfaceArea = 0;
+            double Volume = 0;
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+
+                Box box = new(lenght, width, height);
+
+                surfaceArea = box.SurfaceArea();
+                lateralSurfaceArea = box.LateralSurfaceArea();
+                Volume = box.Volume();
+
+                Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {Volume:f2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+    
+        }
+    }
+}
