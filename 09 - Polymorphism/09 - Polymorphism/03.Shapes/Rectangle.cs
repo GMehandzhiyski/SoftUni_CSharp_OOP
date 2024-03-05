b3e18cdd1f66ec2146bb095e9cbdd0be8e5027c5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-03-05 09:52:12 +0200

    03.Shapes 100% done

---
+++ 09 - Polymorphism/09 - Polymorphism/03.Shapes/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height { get; protected set; }
+        public double Width { get; protected set; }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Height + Width);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
