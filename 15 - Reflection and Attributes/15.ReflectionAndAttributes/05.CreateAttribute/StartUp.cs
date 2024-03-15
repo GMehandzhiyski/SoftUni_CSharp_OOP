c9ee7ea32f356923ab1f27f3d2bdcaed2124b882
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-03-15 12:16:19 +0200

    05.CreateAttribute 100% done

---
+++ 15 - Reflection and Attributes/15.ReflectionAndAttributes/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthorProblem
+{
+    [AuthorAttribute("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Work");
+        }
+    }
+}
