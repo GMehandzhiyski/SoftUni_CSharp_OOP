f2789f2c17b937086efa0fbfc2ee27f1f7f6f0f5
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-03-16 11:38:59 +0200

    02.ValidationAttributes 66% done

---
+++ 16 - Reflection and Attributes - Exercise/08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationAttributes.Atribute;
+
+namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+        private const int minValue = 12;
+        private const int maxValue = 90;
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+                
+        }
+
+        [MyRequiredAttribute]
+        public string Name { get; set; }
+        [MyRange(minValue, maxValue)]
+        public int Age { get; set; }
+    }
+}
---
+++ 16 - Reflection and Attributes - Exercise/08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/ValidationAttributes/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+           Type type = obj.GetType();
+
+             
+            return default;
+        }
+    }
+}
