a170878e0e52a5512b08d566b66bca2903d11bbb
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-03-17 07:59:35 +0200

    Dummy Test 100% done

---
+++ 17 - Unit Testing/09.CSharp-OOP-Unit-Testing-Lab-Skeleton_6.0/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+
+        private Dummy dummy;
+
+        [SetUp]
+        public void SetUp()
+        {
+ 
+            dummy = new Dummy(5, 2);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(dummy.Health, 5);
+         
+        }
+
+        [Test]
+        public void Test2()
+        {
+            dummy.TakeAttack(3);
+            Assert.AreEqual(dummy.Health, 2);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            dummy.TakeAttack(6);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.TakeAttack(1);
+            });
+        }
+
+        [Test]
+        public void Test4()
+        {
+            dummy.TakeAttack(5);
+
+            Assert.AreEqual(dummy.GiveExperience(), 2);
+        }
+    }
+}
